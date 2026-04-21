a6dae9d65308dad2faf548becd6428d39dfd2730
Kabir Virk <203725957+kabirvirk1206@users.noreply.github.com>
2026-04-21 17:46:11 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int counter = 0;;
+        int max = 0;
+       for (unsigned int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 1) {
+            counter++;
+        } else {
+            if (counter > max) {
+                max = counter;
+                
+            }
+
+            counter = 0;
+        }
+
+       }
+
+       if (counter > max){ 
+        max = counter;
+       }
+       
+       return max;
+}
+};
