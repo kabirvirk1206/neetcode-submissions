b005db1a3d8ea2ba355feb4270e2515bde66b967
Kabir Virk <203725957+kabirvirk1206@users.noreply.github.com>
2026-04-23 15:47:42 -0400

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> answer = {};
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (i != j) {
+                    if (nums[i] + nums[j] == target) {
+                        if (i > j) {
+                            answer = {j, i};
+                            return answer;
+                        } else {
+                            answer = {i, j};
+                            return answer;
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
